7d09faffbb49053aafcbd0d67dffb5e706fc10a0
Roman <r.vaseev@crystalnix.com>
2013-07-30 15:28:45 +0700

    Initial commit

--- Books/login/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import logout
-from django.shortcuts import redirect
-from Books.views import main_view
-
-def logout_view(request):
-    logout(request)
-    return redirect(main_view)
-
-
--- Books/profile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-import views
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^$', views.profile,name='profile'),
-    url(r'^change/$', views.profile_change,name='change'),
-    )
