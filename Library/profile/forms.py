3ba183822455223834c7b880dbc0d0347b71eee5
Roman Usynin <romanusynin@crystals-Mac-mini.local>
2013-11-21 15:57:02 +0700

    Redesign Profile form

--- Library/profile/forms.py
+++ Library/profile/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from Library.book_library.models import Book
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions, FieldWithButtons, InlineField, StrictButton
 
 
 class AskReturnForm(forms.Form):
@@ -11,7 +14,27 @@ class AskReturnForm(forms.Form):
 
 
 class ProfileForm(ModelForm):
+    first_name=forms.CharField(max_length=30,required=True,widget=forms.TextInput(attrs={'placeholder': 'First name'}))
+    last_name=forms.CharField(max_length=30,required=True,widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name']
+
+    helper = FormHelper()
+    helper.form_class = 'form-signin'
+    helper.form_show_labels=False
+    helper.layout = Layout(
+            Field('first_name'),
+            Field('last_name'),
+            FormActions(
+
+                    Submit('save_changes', 'Save', css_class='btn btn-lg btn-success'),
+                    Submit('cancel', 'Cancel', css_class='btn btn-lg btn-danger '),
+                    css_class='btn-group  btn-group-lg',
+                    id='id-action-form-change',
+
+            )
+    )
+
+
